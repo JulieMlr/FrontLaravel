2a0fb2e6b46b0a1f69f557f2438d087d74841ca3
JulieMlr <julie.miler.jm@gmail.com>
2021-06-17 14:54:16 +0200

    [UPD] url + build

--- app-laravel/routes/web.php
+++ app-laravel/routes/web.php
@@ -13,10 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
+
+Route::get('/home', function() {
+    return view('welcome');
 });
 
-Route::get('/{any}', function ($any) {
+Route::fallback( function() {
     return view('index');
-})->where('any', '.*');
+});
--- app-vue/vue.config.js
+++ app-vue/vue.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-    outputDir: path.resolve(__dirname, '../app-laravel/public/dist'),
+    outputDir: path.resolve('../app-laravel/public/dist'),
     publicPath: '/dist',
-    indexPath: path.resolve(__dirname, '../app-laravel/resources/views/index.html')
+    indexPath: path.resolve('../app-laravel/resources/views/index.html')
 }
